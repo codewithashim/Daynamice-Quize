1470231b4c60954440d36229d828e321190fb949
Ashim Rudra Paul <codewithashim@gmail.com>
2023-06-24 18:59:00 +0600

    update

--- src/Components/Home/QuizeFrom/QuizeFrom.jsx
+++ src/Components/Home/QuizeFrom/QuizeFrom.jsx
@@ -1,61 +1,73 @@
 "use client"
 import React, { useState } from 'react';
-import Router from 'next/router';
 
 const QuizeFrom = () => {
   const [name, setName] = useState('');
   const [topic, setTopic] = useState('');
-  
+ 
   const apiKey = process.env.API_KEY;
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Call OpenAI API to fetch quiz questions based on the entered topic
-    const response = await fetch('https://api.openai.com/v1/quiz', {
+    // Call your server endpoint to fetch quiz questions based on the entered topic
+    const response = await fetch('http://localhost:5000/api/v1/quiz', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        // Add your OpenAI API key here
-        'Authorization': `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        context: topic,
-        questions: 10, // Number of quiz questions you want
+        topic: topic, // Pass the entered topic from your form
       }),
     });
+    const data = await response.json();
+    console.log(response)
 
-    if (response.ok) {
-      const data = await response.json();
-  
+    if (data) {
+      console.log(data)
       // Store the quiz data in localStorage or pass it to the next page using state management libraries like Redux, MobX, etc.
       localStorage.setItem('quiz', JSON.stringify(data));
+      localStorage.setItem('name', name);
+
+      // Redirect to the quiz page
+      window.location.href = '/quize';
   
-      // Navigate to the quiz page
-      Router.push('/allQuize');
     } else {
       // Handle API error here
-      console.error('Failed to fetch quiz questions from the API.');
+      console.error('Failed to fetch quiz questions from the API.', data.error);
     }
   };
 
-  
 
   return (
     <section>
-      <form className='mainQuizeFrom'>
+      <form className='mainQuizeFrom' onSubmit={handleSubmit}>
         <div className="quizeFormData">
           <div className="quizeFormInput">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Enter Your Name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter Your Name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
           </div>
           <div className="quizeFormInput">
             <label htmlFor="searchInput">Search Now</label>
-            <input type="text" name="searchInput" id="searchInput" placeholder="Type Somthing" />
+            <input
+              type="text"
+              name="searchInput"
+              id="searchInput"
+              placeholder="Type Quize Topics"
+              value={topic}
+              onChange={(event) => setTopic(event.target.value)}
+            />
           </div>
 
           <div className="quizeButton">
-            <button type="submit" className='quizeBtn'>Submit</button>
+            <button type="submit" className='quizeBtn'>Start Qize</button>
           </div>
         </div>
       </form>
--- src/Components/QuizeList/QuizeList.jsx
+++ src/Components/QuizeList/QuizeList.jsx
@@ -1,11 +1,337 @@
-import React from 'react';
+import React from "react";
 
 const QuizeList = () => {
-    return (
-        <section>
-            <h1>Quize List</h1>
-        </section>
-    );
+  return (
+    <section className="container">
+      <div className="mainQuizeContainer container">
+        <div className="quizeContain">
+          <ol className="quizeQuistenSection">
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+            <li className="quizeQuisten">
+              <h4>Quize Quisten</h4>
+              <div className="answerOpction">
+                <ol type="a">
+                  <li>
+                    <label className="block mb-2 mx-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 1" />
+                      Answer 1
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 2" />
+                      Answer 2
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 3" />
+                      Answer 3
+                    </label>
+                  </li>
+                  <li>
+                    <label className="block mb-2">
+                      <input type="radio" name="quizeAnswer" value="Answer 4" />
+                      Answer 4
+                    </label>
+                  </li>
+                </ol>
+              </div>
+            </li>
+          </ol>
+
+          <div className="quizeButton">
+            <button type="submit" className="quizeBtnSubmit">
+              Done
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default QuizeList;
